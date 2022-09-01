22dcc463027cf3ac3d8fe09357ebe71a6637e529
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2022-09-01 17:47:44 -0400

    Add files via upload

--- Phase_SDL.h
+++ Phase_SDL.h
@@ -3,7 +3,21 @@ extern uint8 RAM[];
 void Phase_SDL() {
 	printf("%x\n", RAM[0x0200]);
 	if (RAM[0x0200] == 1) {
-		puts("------------ ok");
+		switch(RAM[0x0201]) {
+			case 1:
+				make_window();
+				break;
+			default:
+				puts("--------- ok");
+			}
+		
+		
+		
 		RAM[0x0200] = 0; }
 
 }
+
+void make_window() {
+	puts("window");
+}
+
