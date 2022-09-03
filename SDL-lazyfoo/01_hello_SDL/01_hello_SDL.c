0d83176be251530bbb0c676ae3bbc11f16bad341
Lawrence Dumaguing <os2nlinux@gmail.com>
2022-09-03 17:03:26 -0400

    17:03

--- SDL-lazyfoo/01_hello_SDL/01_hello_SDL.c
+++ SDL-lazyfoo/01_hello_SDL/01_hello_SDL.c
@@ -4,10 +4,10 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main( int argc, char* args[] ) {
-	SDL_Window* window = NULL;
-	SDL_Surface* screenSurface = NULL;
+SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
 
+int main( int argc, char* args[] ) {
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() ); }
 	else {
