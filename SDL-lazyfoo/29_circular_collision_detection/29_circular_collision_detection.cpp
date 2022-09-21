c85197efc825688eb8e68f33c7bf9dd024545fa9
Lawrence Dumaguing <os2nlinux@gmail.com>
2022-09-21 00:04:42 -0400

    00:04

--- SDL-lazyfoo/29_circular_collision_detection/29_circular_collision_detection.cpp
+++ SDL-lazyfoo/29_circular_collision_detection/29_circular_collision_detection.cpp
@@ -76,461 +76,258 @@ LTexture::LTexture() {
 	mWidth = 0;
 	mHeight = 0; } // LTexture::LTexture
 
-LTexture::~LTexture()
-{
-	
-	free();
-}
+LTexture::~LTexture() {
+	free(); } // LTexture::~LTexture
 
-bool LTexture::loadFromFile( std::string path )
-{
-	
+bool LTexture::loadFromFile( std::string path ) {
 	free();
 
-	
 	SDL_Texture* newTexture = NULL;
 
-	
 	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-	if( loadedSurface == NULL )
-	{
-		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
-	}
-	else
-	{
-		
+	if( loadedSurface == NULL ) {
+		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() ); }
+	else {
 		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
 
-		
-        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
-		if( newTexture == NULL )
-		{
-			printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
-		}
-		else
-		{
-			
+		newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+		if( newTexture == NULL ) {
+			printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() ); }
+		else {
 			mWidth = loadedSurface->w;
-			mHeight = loadedSurface->h;
-		}
+			mHeight = loadedSurface->h; }
 
-		
-		SDL_FreeSurface( loadedSurface );
-	}
+		SDL_FreeSurface( loadedSurface ); }
 
-	
 	mTexture = newTexture;
-	return mTexture != NULL;
-}
+	return mTexture != NULL; } // bool LTexture::loadFromFile
 
 #if defined(SDL_TTF_MAJOR_VERSION)
-bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColor )
-{
-	
+bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColor ) {
 	free();
 
-	
 	SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, textureText.c_str(), textColor );
-	if( textSurface != NULL )
-	{
-		
-        mTexture = SDL_CreateTextureFromSurface( gRenderer, textSurface );
-		if( mTexture == NULL )
-		{
-			printf( "Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError() );
-		}
-		else
-		{
-			
+	if ( textSurface != NULL ) {
+		mTexture = SDL_CreateTextureFromSurface( gRenderer, textSurface );
+		if( mTexture == NULL ) {
+			printf( "Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError() ); }
+		else {
 			mWidth = textSurface->w;
-			mHeight = textSurface->h;
-		}
+			mHeight = textSurface->h; }
 
-		
-		SDL_FreeSurface( textSurface );
-	}
-	else
-	{
-		printf( "Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError() );
-	}
+		SDL_FreeSurface( textSurface ); }
+	else {
+		printf( "Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError() ); }
 
-	
-	
-	return mTexture != NULL;
-}
+	return mTexture != NULL; } // bool LTexture::loadFromRenderedText
 #endif
 
-void LTexture::free()
-{
-	
-	if( mTexture != NULL )
-	{
+void LTexture::free() {
+	if( mTexture != NULL ) {
 		SDL_DestroyTexture( mTexture );
 		mTexture = NULL;
 		mWidth = 0;
-		mHeight = 0;
-	}
-}
+		mHeight = 0; }} // void LTexture::free
 
-void LTexture::setColor( Uint8 red, Uint8 green, Uint8 blue )
-{
-	
-	SDL_SetTextureColorMod( mTexture, red, green, blue );
-}
+void LTexture::setColor( Uint8 red, Uint8 green, Uint8 blue ) {
+	SDL_SetTextureColorMod( mTexture, red, green, blue ); } // void LTexture::setColor
 
-void LTexture::setBlendMode( SDL_BlendMode blending )
-{
-	
-	SDL_SetTextureBlendMode( mTexture, blending );
-}
+void LTexture::setBlendMode( SDL_BlendMode blending ) {
+	SDL_SetTextureBlendMode( mTexture, blending ); } // void LTexture::setBlendMode
 		
-void LTexture::setAlpha( Uint8 alpha )
-{
-	
-	SDL_SetTextureAlphaMod( mTexture, alpha );
-}
+void LTexture::setAlpha( Uint8 alpha ) {
+	SDL_SetTextureAlphaMod( mTexture, alpha ); } // void LTexture::setAlpha
 
-void LTexture::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip )
-{
-	
+void LTexture::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip ) {
 	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
 
-	
-	if( clip != NULL )
-	{
+	if( clip != NULL ) {
 		renderQuad.w = clip->w;
-		renderQuad.h = clip->h;
-	}
+		renderQuad.h = clip->h; }
 
-	
-	SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad, angle, center, flip );
-}
+	SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad, angle, center, flip ); } // void LTexture::render
 
-int LTexture::getWidth()
-{
-	return mWidth;
-}
+int LTexture::getWidth() {
+	return mWidth; } // int LTexture::getWidth
 
-int LTexture::getHeight()
-{
-	return mHeight;
-}
+int LTexture::getHeight() {
+	return mHeight; } // int LTexture::getHeight
 
-Dot::Dot( int x, int y )
-{
-    
-    mPosX = x;
-    mPosY = y;
-
-	
+Dot::Dot( int x, int y ) {
+	mPosX = x;
+	mPosY = y;
 	mCollider.r = DOT_WIDTH / 2;
 
-    
-    mVelX = 0;
-    mVelY = 0;
+	mVelX = 0;
+	mVelY = 0;
 
-	
-	shiftColliders();
-}
+	shiftColliders(); } // Dot::Dot
 
-void Dot::handleEvent( SDL_Event& e )
-{
-    
-	if( e.type == SDL_KEYDOWN && e.key.repeat == 0 )
-    {
-        
-        switch( e.key.keysym.sym )
-        {
-            case SDLK_UP: mVelY -= DOT_VEL; break;
-            case SDLK_DOWN: mVelY += DOT_VEL; break;
-            case SDLK_LEFT: mVelX -= DOT_VEL; break;
-            case SDLK_RIGHT: mVelX += DOT_VEL; break;
-        }
-    }
-    
-    else if( e.type == SDL_KEYUP && e.key.repeat == 0 )
-    {
-        
-        switch( e.key.keysym.sym )
-        {
-            case SDLK_UP: mVelY += DOT_VEL; break;
-            case SDLK_DOWN: mVelY -= DOT_VEL; break;
-            case SDLK_LEFT: mVelX += DOT_VEL; break;
-            case SDLK_RIGHT: mVelX -= DOT_VEL; break;
-        }
-    }
-}
+void Dot::handleEvent( SDL_Event& e ) {
+	if( e.type == SDL_KEYDOWN && e.key.repeat == 0 ) {
+		switch( e.key.keysym.sym ) {
+			case SDLK_UP: mVelY -= DOT_VEL; break;
+			case SDLK_DOWN: mVelY += DOT_VEL; break;
+			case SDLK_LEFT: mVelX -= DOT_VEL; break;
+			case SDLK_RIGHT: mVelX += DOT_VEL; break; }}
+	else if( e.type == SDL_KEYUP && e.key.repeat == 0 ) {
+		switch( e.key.keysym.sym ) {
+			case SDLK_UP: mVelY += DOT_VEL; break;
+			case SDLK_DOWN: mVelY -= DOT_VEL; break;
+			case SDLK_LEFT: mVelX += DOT_VEL; break;
+			case SDLK_RIGHT: mVelX -= DOT_VEL; break; }}} // void Dot::handleEvent
 
-void Dot::move( SDL_Rect& square, Circle& circle )
-{
-    
-    mPosX += mVelX;
+void Dot::move( SDL_Rect& square, Circle& circle ) {
+	mPosX += mVelX;
 	shiftColliders();
 
-    
-	if( ( mPosX - mCollider.r < 0 ) || ( mPosX + mCollider.r > SCREEN_WIDTH ) || checkCollision( mCollider, square ) || checkCollision( mCollider, circle ) )
-    {
-        
-        mPosX -= mVelX;
-		shiftColliders();
-    }
+	if( ( mPosX - mCollider.r < 0 ) || ( mPosX + mCollider.r > SCREEN_WIDTH ) || checkCollision( mCollider, square ) || checkCollision( mCollider, circle ) ) {
+		mPosX -= mVelX;
+		shiftColliders(); }
 
-    
-    mPosY += mVelY;
+	mPosY += mVelY;
 	shiftColliders();
 
-    
-    if( ( mPosY - mCollider.r < 0 ) || ( mPosY + mCollider.r > SCREEN_HEIGHT ) || checkCollision( mCollider, square ) || checkCollision( mCollider, circle ) )
-    {
-        
-        mPosY -= mVelY;
-		shiftColliders();
-    }
-}
+	if( ( mPosY - mCollider.r < 0 ) || ( mPosY + mCollider.r > SCREEN_HEIGHT ) || checkCollision( mCollider, square ) || checkCollision( mCollider, circle ) ) {
+		mPosY -= mVelY;
+		shiftColliders(); }} // void Dot::move
 
-void Dot::render()
-{
-    
-	gDotTexture.render( mPosX - mCollider.r, mPosY - mCollider.r );
-}
+void Dot::render() {
+	gDotTexture.render( mPosX - mCollider.r, mPosY - mCollider.r ); } // void Dot::render
 
-Circle& Dot::getCollider()
-{
-	return mCollider;
-}
+Circle& Dot::getCollider() {
+	return mCollider; } // Circle& Dot::getCollider
 
-void Dot::shiftColliders()
-{
-	
+void Dot::shiftColliders() {
 	mCollider.x = mPosX;
-	mCollider.y = mPosY;
-}
+	mCollider.y = mPosY; } // void Dot::shiftColliders
 
-bool init()
-{
-	
+bool init() {
 	bool success = true;
 
-	
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
 		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
-	}
-	else
-	{
-		
-		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-		{
-			printf( "Warning: Linear texture filtering not enabled!" );
-		}
-
+		success = false; }
+	else {
+		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) ) {
+			printf( "Warning: Linear texture filtering not enabled!" ); }
 		
 		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
+		if( gWindow == NULL ) {
 			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			
+			success = false; }
+		else {
 			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-			if( gRenderer == NULL )
-			{
+			if( gRenderer == NULL ) {
 				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-				success = false;
-			}
-			else
-			{
-				
+				success = false; }
+			else {
 				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 
-				
 				int imgFlags = IMG_INIT_PNG;
-				if( !( IMG_Init( imgFlags ) & imgFlags ) )
-				{
+				if( !( IMG_Init( imgFlags ) & imgFlags ) ) {
 					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-					success = false;
-				}
-			}
-		}
-	}
+					success = false; }}}}
 
-	return success;
-}
+	return success; } // bool init
 
-bool loadMedia()
-{
-	
+bool loadMedia() {
 	bool success = true;
 
-	
-	if( !gDotTexture.loadFromFile( "dot.bmp" ) )
-	{
+	if( !gDotTexture.loadFromFile( "dot.bmp" ) ) {
 		printf( "Failed to load dot texture!\n" );
-		success = false;
-	}
+		success = false; }
 
-	return success;
-}
+	return success; } // bool loadMedia
 
-void close()
-{
-	
+void close() {
 	gDotTexture.free();
 
-	
 	SDL_DestroyRenderer( gRenderer );
 	SDL_DestroyWindow( gWindow );
 	gWindow = NULL;
 	gRenderer = NULL;
 
-	
 	IMG_Quit();
-	SDL_Quit();
-}
+	SDL_Quit(); } // void close
 
-bool checkCollision( Circle& a, Circle& b )
-{
-	
+bool checkCollision( Circle& a, Circle& b ) {
 	int totalRadiusSquared = a.r + b.r;
 	totalRadiusSquared = totalRadiusSquared * totalRadiusSquared;
 
-    
-    if( distanceSquared( a.x, a.y, b.x, b.y ) < ( totalRadiusSquared ) )
-    {
-        
-        return true;
-    }
+	if( distanceSquared( a.x, a.y, b.x, b.y ) < ( totalRadiusSquared ) ) {
+		return true; }
 
-    
-    return false;
-}
+    return false; } // bool checkCollision
 
-bool checkCollision( Circle& a, SDL_Rect& b )
-{
-    
-    int cX, cY;
+bool checkCollision( Circle& a, SDL_Rect& b ) {
+	int cX, cY;
 
-    
-    if( a.x < b.x )
-    {
-        cX = b.x;
-    }
-    else if( a.x > b.x + b.w )
-    {
-        cX = b.x + b.w;
-    }
-    else
-    {
-        cX = a.x;
-    }
+	if( a.x < b.x ) {
+		cX = b.x; }
+	else if( a.x > b.x + b.w ) {
+		cX = b.x + b.w; }
+	else {
+		cX = a.x; }
 
-    
-    if( a.y < b.y )
-    {
-        cY = b.y;
-    }
-    else if( a.y > b.y + b.h )
-    {
-        cY = b.y + b.h;
-    }
-    else
-    {
-        cY = a.y;
-    }
+	if( a.y < b.y ) {
+		cY = b.y; }
+	else if( a.y > b.y + b.h ) {
+		cY = b.y + b.h; }
+	else {
+		cY = a.y; }
 
-    
-    if( distanceSquared( a.x, a.y, cX, cY ) < a.r * a.r )
-    {
-        
-        return true;
-    }
+	if( distanceSquared( a.x, a.y, cX, cY ) < a.r * a.r ) {
+		return true; }
 
-    
-    return false;
-}
+	return false; } // bool checkCollision
 
-double distanceSquared( int x1, int y1, int x2, int y2 )
-{
+double distanceSquared( int x1, int y1, int x2, int y2 ) {
 	int deltaX = x2 - x1;
 	int deltaY = y2 - y1;
-	return deltaX*deltaX + deltaY*deltaY;
-}
+	return deltaX*deltaX + deltaY*deltaY; } // double distanceSquared
 
-int main( int argc, char* args[] )
-{
-	
-	if( !init() )
-	{
-		printf( "Failed to initialize!\n" );
-	}
-	else
-	{
-		
-		if( !loadMedia() )
-		{
-			printf( "Failed to load media!\n" );
-		}
-		else
-		{	
-			
+// ***************************************************************************************
+int main( int argc, char* args[] ) {
+	if( !init() ) {
+		printf( "Failed to initialize!\n" ); }
+	else {
+		if( !loadMedia() ) {
+			printf( "Failed to load media!\n" ); }
+		else {
 			bool quit = false;
 
-			
 			SDL_Event e;
 
-			
 			Dot dot( Dot::DOT_WIDTH / 2, Dot::DOT_HEIGHT / 2 );
 			Dot otherDot( SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4 );
 
-			
 			SDL_Rect wall;
 			wall.x = 300;
 			wall.y = 40;
 			wall.w = 40;
 			wall.h = 400;
-			
-			
-			while( !quit )
-			{
-				
-				while( SDL_PollEvent( &e ) != 0 )
-				{
-					
-					if( e.type == SDL_QUIT )
-					{
-						quit = true;
-					}
 
-					
-					dot.handleEvent( e );
-				}
+			while( !quit ) {
+				while( SDL_PollEvent( &e ) != 0 ) {
+					if( e.type == SDL_QUIT ) {
+						quit = true; }
 
-				
+					dot.handleEvent( e ); }
+
 				dot.move( wall, otherDot.getCollider() );
 
-				
 				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 				SDL_RenderClear( gRenderer );
 
-				
 				SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );		
 				SDL_RenderDrawRect( gRenderer, &wall );
-				
-				
+
 				dot.render();
 				otherDot.render();
 
-				
-				SDL_RenderPresent( gRenderer );
-			}
-		}
-	}
+				SDL_RenderPresent( gRenderer ); }}}
 
-	
 	close();
 
-	return 0;
-}
+	return 0; }
